fea890166c10678cc7a89179d5dd24fb1a5b6277
Mateusz Hołenko <mholenko@antmicro.com>
2015-01-12 12:57:01 +0100

    CLinkerTask added.
    This commit adds task that can be used to link already compiled
    object files.

---
+++ CCTask/CLinkerTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using System.IO;
+using Microsoft.Build.Framework;
+using System.Linq;
+
+namespace CCTask
+{
+	public class CLinkerTask : Task
+	{
+		[Required]
+		public ITaskItem[] ObjectFiles { get; set; }
+
+		public ITaskItem[] Flags { get; set; }
+
+		[Required]
+		public string Output { get; set; }
+
+		public override bool Execute()
+		{
+			Logger.Instance = new XBuildLogProvider(Log); // TODO: maybe initialise statically; this put in constructor causes NRE 
+
+			var buildPath = Path.GetDirectoryName(Output);
+			buildPath = buildPath == string.Empty ? Directory.GetCurrentDirectory() : Path.GetFullPath(buildPath);
+			var ofiles = ObjectFiles.Select(x => Path.GetFullPath(x.ItemSpec));
+			// linking
+			var linker = CompilerProvider.Instance.CLinker;
+			return linker.Link(ofiles, Path.Combine(buildPath, Output), Flags.Aggregate(string.Empty, (curr, next) => string.Format("{0} {1}", curr, next.ItemSpec)), (x, y) => true);
+		}
+	}
+}
+
