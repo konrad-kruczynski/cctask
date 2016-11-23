119a783b75683e17cd6d127e14a24c14bdf19ec4
Mateusz Hołenko <mholenko@antmicro.com>
2016-11-23 15:51:15 +0100

    Extend environment task.

--- CCTask/EnvironmentTask.cs
+++ CCTask/EnvironmentTask.cs
@@ -7,11 +7,15 @@ namespace CCTask
 	public class EnvironmentTask : Task
 	{
 		[Output]
-		public int PointerSize { get; private set; }
+		public int OsPointerSize { get; private set; }
+
+		[Output]
+		public int FrameworkPointerSize { get; private set; }
 
 		public override bool Execute()
 		{
-			PointerSize = Environment.Is64BitOperatingSystem ? 64 : 32;
+			FrameworkPointerSize = IntPtr.Size * 8;
+			OsPointerSize = Environment.Is64BitOperatingSystem ? 64 : 32;
 			return true;
 		}
 
